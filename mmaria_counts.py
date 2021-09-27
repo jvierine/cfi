05d696c58e2ae751c24d9ab2623bdd642307d461
Juha Vierinen <jvierine@gmail.com>
2021-09-27 07:49:35 +0000

    plot counts

--- mmaria_counts.py
+++ mmaria_counts.py
@@ -5,36 +5,33 @@ import numpy as n
 import matplotlib.pyplot as plt
 import h5py
 import datetime
+import cfi_config as c
 
-fl=glob.glob("*.h5")
+fl=glob.glob("%s/*.h5"%(c.data_directory))
 fl.sort()
 n_files=len(fl)
 print(n_files)
 links=[b"Alta_Alta",b"Andenes_Andenes",b"Andenes_Straumen",b"Tromso_Tromso"]
-plot_all=False
+plot_all=True
 
 for link in links:
-    print(link)
     counts=[]
-    countso=[]
     dates=[]
-    
+
     for f in fl:
         h=h5py.File(f,"r")
 
         idx=n.where(h["link"][()]==link)[0]
         if len(idx)> 0:
             counts.append(len(n.unique(h["t"][()][idx])))
-            countso.append(len(h["t"][()][idx]))
+            # smallest time in file
             dt_object = datetime.datetime.fromtimestamp(n.min(h["t"][()][idx]))
             dates.append(dt_object)
-
         h.close()
         
-    if plot_all:
-        plt.plot(dates,countso,label=str(link))
-    else:
-        plt.plot(dates,counts,label=str(link))
+    plt.plot(dates,counts,label=str(link))
+
+
 
 plt.legend()
 plt.xlabel("Date (UTC)")
