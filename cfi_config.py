0293accd49bb5a3333ebc428a3f0deb73bfdb728
Juha Vierinen <jvierine@gmail.com>
2023-06-06 14:42:12 +0000

    added cfg

--- cfi_config.py
+++ cfi_config.py
@@ -3,6 +3,7 @@
 from configparser import ConfigParser
 import sys
 import json
+import numpy as n
 
 if len(sys.argv) != 2:
     print("provide config file argument")
@@ -15,6 +16,27 @@ lon=float(json.loads(c["cfi"]["lon"]))
 
 data_directory=json.loads(c["cfi"]["data_directory"])
 plot_directory=json.loads(c["cfi"]["plot_directory"])
+
+# some parameters with default values
+dcos_thresh=0.8
+if "dcos_thresh" in c["cfi"].keys():
+    dcos_thresh=float(json.loads(c["cfi"]["dcos_thresh"]))
+    
+debug_epsilon_fit=False
+if "debug_epsilon_fit" in c["cfi"].keys():
+    debug_epsilon_fit=bool(json.loads(c["cfi"]["debug_epsilon_fit"]))
+
+horizontal_correlation_n_days=1
+if "horizontal_correlation_n_days" in c["cfi"].keys():
+    horizontal_correlation_n_days=float(json.loads(c["cfi"]["horizontal_correlation_n_days"]))
+
+horizontal_correlation_post_avg=1
+if "horizontal_correlation_post_avg" in c["cfi"].keys():
+    horizontal_correlation_post_avg=int(json.loads(c["cfi"]["horizontal_correlation_post_avg"]))
+    
+horizontal_correlation_heights=n.array(json.loads(c["cfi"]["horizontal_correlation_heights"]))
+horizontal_correlation_dh=float(json.loads(c["cfi"]["horizontal_correlation_dh"]))
+
 
 #data_directory="/data0/SIMONe_multilink/JRO"
 #plot_directory="/data0/SIMONe_multilink/JRO/"
@@ -29,3 +51,5 @@ plot_directory=json.loads(c["cfi"]["plot_directory"])
 #lon=19.0
 
 
+if __name__ == "__main__":
+    print(c)
--- hor_acf_daily.py
+++ hor_acf_daily.py
@@ -37,7 +37,7 @@ def avg_hor_acfs(md, # data
     #
     # daily averages of horizontal correlation functions, save them for post processing
     #
-    os.system("mkdir -p mpi/%s"%(name))
+    os.system("mkdir -p %s/%s"%(c.data_directory,name))
 
     n_lags=len(s_h)
     all_acfs=[]
@@ -47,26 +47,53 @@ def avg_hor_acfs(md, # data
     
     # we need a bit of extra meteors before and after for the mean wind.
     dw=md.read_data(t0=t0-2*3600,t1=t1+2*3600)
-    d=md.read_data(t0=t0,t1=t1)    
-    n_meas=len(d["t"])
+    #d=md.read_data(t0=t0,t1=t1)    
+    n_meas=len(dw["t"])
     print("n_meteors %d"%(n_meas))
     if n_meas > 100:
-        times,times_h,v,ve,rgs,lat0,lon0,dt,dh,resid=mw.mean_wind_grad(meas=dw,
-                                                                       times=mean_wind_time,
-                                                                       dt=mean_wind_time_avg,
-                                                                       dh=1.0,
-                                                                       dz=3.0,
-                                                                       max_alt=105,
-                                                                       min_alt=78,
-                                                                       dcos_thresh=dcos_thresh,
-                                                                       ofname="res/tmp-%03d.h5"%(rank),
-                                                                       outlier_sigma=4,
-                                                                       gradients=False,
-                                                                       debug=False,
-                                                                       data='dict')
+        #times,times_h,v,ve,rgs,lat0,lon0,dt,dh,resid=
+        mwr=mw.mean_wind_grad(meas=dw,
+                              times=mean_wind_time,
+                              dt=mean_wind_time_avg,
+                              dh=1.0,
+                              dz=3.0,
+                              max_alt=105,
+                              min_alt=78,
+                              dcos_thresh=dcos_thresh,
+                              ofname="res/tmp-%03d.h5"%(rank),
+                              outlier_sigma=4,
+                              gradients=False,
+                              debug=False)
+
+        # only use ones that are not outliers to wind model
+        # and that are in the correct time range
+        gidx=mwr["good_idx"]
+        dw["t"]=dw["t"][gidx]
+        dw["alpha_norm"]=dw["alpha_norm"][gidx]
+        dw["braggs"]=dw["braggs"][gidx,:]
+        dw["dcos"]=dw["dcos"][gidx,:]
+        dw["dop_errs"]=dw["dop_errs"][gidx]
+        dw["dops"]=dw["dops"][gidx]
+        dw["heights"]=dw["heights"][gidx]
+        dw["lats"]=dw["lats"][gidx]
+        dw["lons"]=dw["lons"][gidx]
+        dw["link"]=dw["link"][gidx]
         
-            
-        meas=cfi.get_meas(meas_file=d,
+        # then filter for those in correct time range, because we had a broader time range when estimating the mean wind
+        gidx=n.where( (dw["t"] > t0) & (dw["t"] < t1))[0]
+        dw["t"]=dw["t"][gidx]
+        dw["alpha_norm"]=dw["alpha_norm"][gidx]
+        dw["braggs"]=dw["braggs"][gidx,:]
+        dw["dcos"]=dw["dcos"][gidx,:]
+        dw["dop_errs"]=dw["dop_errs"][gidx]
+        dw["dops"]=dw["dops"][gidx]
+        dw["heights"]=dw["heights"][gidx]
+        dw["lats"]=dw["lats"][gidx]
+        dw["lons"]=dw["lons"][gidx]
+        dw["link"]=dw["link"][gidx]
+        
+        
+        meas=cfi.get_meas(meas_file=dw,
                           mean_rem=remove_mean,
                           plot_dops=False,
                           dcos_thresh=dcos_thresh,
@@ -83,9 +110,8 @@ def avg_hor_acfs(md, # data
                                                                LTz=True,
                                                                title=name)
 
-        out_fname="mpi/%s/hacf_res-%06d.h5"%(name,t0)
+        out_fname="%s/%s/hacf_res-%06d.h5"%(c.data_directory,name,t0)
         os.system("rm -f %s"%(out_fname))        
-        
         print("writing %s"%(out_fname))
         ho=h5py.File(out_fname,"w")
         ho["acfs"]=acfs
@@ -105,33 +131,34 @@ md=mr.mmaria_data(c.data_directory)#for many files in a directory
 b=md.get_bounds()
 d0=stuffr.unix2date(b[0])
 
-
 n_days = int(n.round((b[1]-b[0])/(24*3600)))
 t0_floor=stuffr.date2unix(d0.year,d0.month,d0.day,0,0,0)
 print("number of days %d floored t0 %1.2f"%(n_days,t0_floor))
 
-# 600 s dtau, 25 km ds_h, 400 km max horizontal lag, 3 day sliding window
-name="daily_600s_5day_90km_LTz"
-
-for day_no in range(rank,n_days,size):
-    # three day running window with one day increment
-    t0 = t0_floor + day_no*24*3600
-    t1 = t0_floor + day_no*24*3600 + 5*24*3600
-
-    # 900 second time step for mean wind, but the mean wind averaging window is four hours
-    mean_wind_time = n.linspace(t0,t1,num=96*5)
-    avg_hor_acfs(md,
-                 dcos_thresh=0.8,
-                 mean_wind_time_avg=4*3600.0,
-                 mean_wind_time=mean_wind_time,
-                 h0=90.0,
-                 dh=6,
-                 ds_h=25.0,            # horizontal distance resolution
-                 dtau=600.0,           # lag time resolution
-                 s_h=n.arange(0.0,400.0,12.5),
-                 ds_z=1,               # vertical lag resolution
-                 t0=t0,
-                 t1=t1,
-                 name=name)
+for h0 in c.horizontal_correlation_heights:
+    # 600 s dtau, 25 km ds_h, 400 km max horizontal lag, 3 day sliding window
+    #name="daily_600s_7day_82km_LTz"
+    name="hcor_%1.2f"%(h0)
+        
+    for day_no in range(rank,n_days,size):
+        # three day running window with one day increment
+        t0 = t0_floor + day_no*24*3600
+        t1 = t0_floor + day_no*24*3600 + c.horizontal_correlation_n_days*24*3600
+        
+        # 900 second time step for mean wind, but the mean wind averaging window is four hours
+        mean_wind_time = n.linspace(t0,t1,num=int(96*c.horizontal_correlation_n_days))
+        avg_hor_acfs(md,
+                     dcos_thresh=c.dcos_thresh,
+                     mean_wind_time_avg=4*3600.0,
+                     mean_wind_time=mean_wind_time,
+                     h0=h0,
+                     dh=c.horizontal_correlation_dh, # look for meteors between [h0-dh/2,h0+dh/2]
+                     ds_h=25.0,            # horizontal distance resolution
+                     dtau=600.0,           # lag time resolution
+                     s_h=n.arange(0.0,400.0,12.5),
+                     ds_z=1,               # vertical lag resolution
+                     t0=t0,
+                     t1=t1,
+                     name=name)
 
 
